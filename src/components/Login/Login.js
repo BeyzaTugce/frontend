ad120914633c9964acb5186c6696bd6adda43020
Mert Yıldırır <ge65mid@mytum.de>
2021-06-20 15:15:00 +0200

    signup page created, a few changes on the login page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,41 +1,56 @@
 import React from 'react';
 import { Container, Row, Col, Jumbotron, Form, Button} from 'react-bootstrap';
-import CategoryBar from "../Header/CategoryBar";
+import { useHistory } from 'react-router-dom';
 
 const Login = () => {
 
+    const history = useHistory();
+
+    const handleSignupClick = () => {
+        history.push("/signup");
+    }
+
     return (
         <div className="Login-page">
-            <div className="Loginform">
-                <Container>
-                    <Row>
-                        <Col md={{ span: 7, offset: 3 }}>
-                            <Jumbotron>
-                                <Form>
-                                    <Form.Group controlId="formBasicEmail">
-                                        <Form.Label>Email address</Form.Label>
-                                        <Form.Control type="email" placeholder="Enter email" />
-                                        <Form.Text className="text-muted">
-                                            We'll never share your email with anyone else.
-                                        </Form.Text>
-                                    </Form.Group>
+            <Container>
+                <Row>
+                    <Col className="mt-5" md={{ span: 7, offset: 3 }}>
+                        <Jumbotron>
+                            <h1>Login</h1>
+                            <Form>
 
-                                    <Form.Group controlId="formBasicPassword">
-                                        <Form.Label>Password</Form.Label>
-                                        <Form.Control type="password" placeholder="Password" />
-                                    </Form.Group>
-                                    <Form.Group controlId="formBasicCheckbox">
-                                        <Form.Check type="checkbox" label="Remember me" />
-                                    </Form.Group>
-                                    <Button variant="primary" type="submit">
-                                        Submit
-                                    </Button>
-                                </Form>
-                            </Jumbotron>
-                        </Col>
-                    </Row>
-                </Container>
-            </div>
+                                {/* Email */}
+                                <Form.Group controlId="formBasicEmail">
+                                    <Form.Label>Email address</Form.Label>
+                                    <Form.Control type="email" placeholder="Enter Email" />
+                                    <Form.Text className="text-muted">
+                                        We'll never share your email with anyone else.
+                                    </Form.Text>
+                                </Form.Group>
+
+                                {/* Password */}
+                                <Form.Group controlId="formBasicPassword">
+                                    <Form.Label>Password</Form.Label>
+                                    <Form.Control type="password" placeholder="Password" />
+                                </Form.Group>
+                                <Form.Group controlId="formBasicCheckbox">
+                                    <Form.Check type="checkbox" label="Remember me" />
+                                </Form.Group>
+
+                                {/* Login and Signup buttons */}
+                                <Button variant="primary" type="submit">
+                                    Login
+                                </Button>
+                                <h3 className="mt-4">Still haven't registered?</h3>
+                                <Button onClick={handleSignupClick} variant="primary">
+                                    Signup
+                                </Button>
+                                
+                            </Form>
+                        </Jumbotron>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,11 +1,114 @@
 import React from 'react';
+import { Container, Row, Col, Jumbotron, Form, Button } from 'react-bootstrap';
 
-const Form = () => {
+const Signup = () => {
+
+
     return (
-        <div>
-            AMAZING SIGN-UP FORM 
+        <div className="Signup-page">
+            <Container>
+                <Row>
+                    <Col className="mt-5" md={{ span: 7, offset: 3 }}>
+                        <Jumbotron>
+                            <h1>Become our customer</h1>
+                            <p>
+                                By registering to our platform, you can create your own garage sale, also start bargaining for zillions of products. 
+                            </p>
+                            <Form>
+
+                                {/* Name */}
+                                <Form.Group>
+                                    <Form.Label>Name</Form.Label>
+                                    <Form.Row>
+                                        <Col>
+                                            <Form.Control placeholder="First Name" />
+                                        </Col>
+                                        <Col>
+                                            <Form.Control placeholder="Last Name" />
+                                        </Col>
+                                    </Form.Row>
+                                </Form.Group>
+
+                                {/* Birthday and Gender */}
+                                <Form.Row>
+                                    <Form.Group xs={8} as={Col} controlId="signupBirth">
+                                        <Form.Label>Birthday</Form.Label>
+                                        <Form.Control type="date" />
+                                    </Form.Group>
+                                    <Form.Group as={Col}>
+                                        <Form.Label>Gender</Form.Label>
+                                            <Form.Control as="select">
+                                                <option>m/f/d</option>
+                                                <option>m</option>
+                                                <option>f</option>
+                                                <option>d</option>
+                                            </Form.Control>
+                                    </Form.Group>
+                                </Form.Row>
+
+                                {/* Email and Phone*/}
+                                <Form.Row>
+                                    <Form.Group as={Col} controlId="formGridEmail">
+                                        <Form.Label>Email</Form.Label>
+                                            <Form.Control type="email" placeholder="Enter Email" />
+                                    </Form.Group>
+                                    <Form.Group as={Col} controlId="formGridPhone">
+                                        <Form.Label>Phone</Form.Label>
+                                            <Form.Control type="telephone" placeholder="Enter Phone Number" />
+                                    </Form.Group>
+                                </Form.Row>
+                                
+                                {/* Password */}
+                                <Form.Group controlId="formGridEmail">
+                                    <Form.Label>Password</Form.Label>
+                                    <Form.Row>                                   
+                                        <Col>
+                                            <Form.Control type="password" placeholder="Password" />
+                                        </Col>
+                                        <Col>
+                                            <Form.Control type="password" placeholder="Re-enter password" />
+                                        </Col>
+                                    </Form.Row>
+                                </Form.Group>
+
+                                {/* Address */}
+                                <Form.Group>
+                                    <Form.Label>Address</Form.Label>
+                                    <Form.Control placeholder="Address line 1 (or Company name)"></Form.Control>
+                                </Form.Group>
+
+                                <Form.Row>
+                                    <Form.Group xs={8} as={Col}>
+                                        <Form.Label>Address 2</Form.Label>
+                                        <Form.Control placeholder="Street and house number"></Form.Control>
+                                    </Form.Group>
+                                    <Form.Group as={Col}>
+                                        <Form.Label>Postcode</Form.Label>
+                                        <Form.Control placeholder="80797"></Form.Control>
+                                    </Form.Group>
+                                </Form.Row>
+
+                                {/* City */}
+                                <Form.Group>
+                                    <Form.Label>Town/City</Form.Label>
+                                    <Form.Control placeholder="Munich"></Form.Control>
+                                </Form.Group>
+
+                                {/* Checkbox for terms and Register button */}
+                                <Form.Group controlId="formBasicCheckbox">
+                                    <Form.Check type="checkbox" label="I agree to the terms and conditions ..." />
+                                </Form.Group>
+                                <Button variant="primary" type="submit">
+                                    Register
+                                </Button>
+                                
+                            </Form>
+                        </Jumbotron>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
 
-export default Form;
+export default Signup;
