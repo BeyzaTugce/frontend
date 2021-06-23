6ca318f4a51e19e2df36b301df3b863fbc82892d
Beyza Bilgic <beyza.bilgic@tum.de>
2021-06-23 15:04:15 +0200

    GarageCreation modified

--- src/components/GarageCreation/GarageCreation.js
+++ src/components/GarageCreation/GarageCreation.js
@@ -1,24 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
-import Header from "../Header/Header";
-import {Button, Col, Container, FormCheck, FormGroup, FormLabel, Row} from "react-bootstrap";
+import {
+    Button,
+    Col,
+    Container,
+    FormCheck,
+    FormGroup,
+    FormLabel,
+    FormControl,
+    Image,
+    ListGroup,
+    ListGroupItem, Nav,
+    Row, TabContent, TabPane, TabContainer
+} from "react-bootstrap";
 import FormCheckInput from "react-bootstrap/FormCheckInput";
-import {Book, PlusLg} from "react-bootstrap-icons";
+import {Book, Clock, PlusLg} from "react-bootstrap-icons";
 import "./GarageCreation.css"
+import logo from "../Header/logo.png";
 
 const GarageCreation = (props) => {
+
 
     return(
 
         <div>
-            <Header />
-            <Container>
-                <Row>
-                    <FormLabel className="myGarage">My Garage</FormLabel>
-                </Row>
-                <Row>
-                    <Col md={{ span: 4}}>
-                        <FormGroup>
+            <h1 className="myGarage text-center">My Garage</h1>
+            <div className="w-100" style={{paddingInline:50}}>
+                <div className="d-flex justify-content-between">
+                    <div className="d-inline-block">
+                        <FormGroup style={{marginTop: 60}}>
                             <div className="garageOptions">
                                 <FormLabel>Garage Sale Options</FormLabel>
                                 <FormCheck>
@@ -44,45 +54,100 @@ const GarageCreation = (props) => {
                                 </FormCheck>
                             </div>
                         </FormGroup>
-                        <div>
-
+                        <div className="d-inline-flex justify-content-center" style={{marginBottom:-20}}>
+                            <div style={{marginRight:10, marginTop:10}}><Clock/></div>
+                            <FormGroup style={{fontStyle: "italic"}}>
+                                <FormLabel>Beginning Date: 16.05.2021</FormLabel>
+                                <br/>
+                                <FormLabel>Ending Date: 16.06.2021</FormLabel>
+                            </FormGroup>
                         </div>
-                    </Col>
-                    <Col md={{ span: 3, offset: 3 }}>
+                    </div>
+                    <div className="d-inline-block" style={{paddingRight: 40, width: 600}}>
                         <Button className="button-rounded">
                             <PlusLg></PlusLg>
                         </Button>
                         <FormLabel className="frame">Add Item</FormLabel>
-                        <FormGroup>
-                            <div className="garageOptions">
-                                <FormLabel>Delivery</FormLabel>
-                                <FormCheck>
-                                    <FormCheckInput isValid />
-                                    <FormCheck.Label>{`Shipment`}</FormCheck.Label>
-                                </FormCheck>
-                                <FormCheck>
-                                    <FormCheckInput isValid />
-                                    <FormCheck.Label>{`Pick-up`}</FormCheck.Label>
-                                </FormCheck>
+                        <ListGroup>
+                            <ListGroupItem className="d-flex align-items-start">
+                                <TabContainer id="left-tabs-example" defaultActiveKey="info">
+                                    <Row>
+                                        <Col sm={3}>
+                                            <Nav variant="pills" className="flex-column">
+                                                <Nav.Item>
+                                                    <Nav.Link eventKey="item">Item Title</Nav.Link>
+                                                </Nav.Item>
+                                                <Nav.Item>
+                                                    <Nav.Link eventKey="info">General Information</Nav.Link>
+                                                </Nav.Item>
+                                                <Nav.Item>
+                                                    <Nav.Link eventKey="price">Price</Nav.Link>
+                                                </Nav.Item>
+                                                <Nav.Item>
+                                                    <Nav.Link eventKey="tag">Tags</Nav.Link>
+                                                </Nav.Item>
+                                            </Nav>
+                                        </Col>
+                                        <Col sm={9}>
+                                            <TabContent>
+                                                <TabPane eventKey="item">
+                                                    <FormControl type="title" placeholder="Enter item title" style={{marginLeft:15}}/>
+                                                </TabPane>
+                                                <TabPane eventKey="info">
+                                                    <FormControl type="info" placeholder="Write about your item" style={{marginLeft:15, marginTop:75}}/>
+                                                </TabPane>
+                                                <TabPane eventKey="price">
+                                                    <FormControl type="price" placeholder="Enter item price" style={{marginLeft:15, marginTop:130}}/>
+                                                </TabPane>
+                                                <TabPane eventKey="tag">
+                                                    <FormControl type="tag" placeholder="Enter some tags" style={{marginLeft:15, marginTop:170}}/>
+                                                </TabPane>
+                                            </TabContent>
+                                        </Col>
+                                    </Row>
+                                </TabContainer>
+                            </ListGroupItem>
+                        </ListGroup>
+
+                    </div>
+                </div>
+
+                <div><FormLabel className="addItems">Add Items</FormLabel></div>
+                <div className="list-whole">
+                    <ListGroup>
+                        <ListGroupItem className="d-inline-flex align-items-center justify-content-between" style={{borderColor: "#85A582"}}>
+                            <FormCheck
+                                type="checkbox"
+                                id="item-checkbox"
+                                style={{marginInline:17, marginRight:30}}
+                            />
+                            <div
+                                className="img-container d-flex align-items-center"
+                                style={{width:100, height:100, textAlign:"center", marginRight:30}}
+                            >
+                                <Image
+                                    className="item-image"
+                                    src={logo}
+                                    fluid
+                                />
                             </div>
-                        </FormGroup>
-                    </Col>
-                </Row>
-                <Row><FormLabel className="addItems">Add Items</FormLabel></Row>
-                <Row>
-                    <div className="addItemTable">
-                        <div className="itemImage"><Book></Book></div>
-                        <FormLabel className="itemName">Some Book</FormLabel>
-                    </div>
-                    <div className="addItemTable">
-                        <FormCheck className="addItemCheckBox">
-                            <FormCheck.Input></FormCheck.Input>
-                        </FormCheck>
-                    </div>
-
-                </Row>
-            </Container>
-
+                            <div className="name-and-tags flex-fill">
+                                <div className="item-name">Some Item</div>
+                                <div className="item-tags text-black-50" style={{fontSize:14}}>#all #tags #here</div>
+                            </div>
+                            <div className="justify-content-end d-inline-flex align-items-center justify-content-end">
+                                <Button
+                                    className='btn'
+                                    variant="dark"
+                                    style={{backgroundColor: "#85A582"}}
+                                >
+                                    Edit
+                                </Button>
+                            </div>
+                        </ListGroupItem>
+                    </ListGroup>
+                </div>
+            </div>
         </div>
 
     );
