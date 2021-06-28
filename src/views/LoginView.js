57bc0c4d63468d3cf5d8fbdc587d605bcdc85cbb
Bikem Çamli <ge37zeb@mytum.de>
2021-06-28 02:37:29 +0200

    login modified

--- src/views/LoginView.js
+++ src/views/LoginView.js
@@ -1,16 +1,39 @@
-import React from 'react';
-import { useHistory } from 'react-router-dom';
-
-import Login from "../components/Login";
+import React, { useEffect } from "react";
+import { withRouter } from "react-router-dom";
+import { connect, useSelector } from "react-redux";
+import LoginComponent from "../components/Login";
 
 import { login } from "../redux/actions/UserActions";
 
-const LoginView = () => {
+function UserLoginView(props) {
+    const user = useSelector((state) => state.user);
 
+    useEffect(() => {
+        if (user.user) {
+            props.history.push("/");
+        }
+    }, [user, props.history]);
+
+    const onLogin = (username, password) => {
+        props.dispatch(login(username, password));
+    };
+
+    const onCancel = () => {
+        props.history.push("/");
+    };
+
+    const onSignUp = () => {
+        props.history.push("/register");
+    };
 
     return (
-        <Login />
+        <LoginComponent
+            user={user}
+            onCancel={onCancel}
+            onLogin={onLogin}
+            onSignUp={onSignUp}
+        />
     );
 }
 
-export default LoginView;
+export default connect()(withRouter(UserLoginView));
