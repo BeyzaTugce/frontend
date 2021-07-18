1dcb852ae224cbe27e3196d2185df6af488818c4
Beyza Bilgic <beyza.bilgic@tum.de>
2021-07-18 17:56:53 +0200

    items added to purchaseview

--- src/views/PurchaseView.js
+++ src/views/PurchaseView.js
@@ -4,12 +4,12 @@ import {connect, useSelector} from "react-redux";
 import {withRouter} from "react-router-dom";
 import {Button, FormLabel} from "react-bootstrap";
 import {getPurchaseSeller, getPurchaseBuyer} from "../redux/actions/PurchaseActions";
-import {getSeller} from "../redux/actions/GarageActions";
 import Header from "../components/Header";
+import GarageItem from "../components/GarageItem";
 
 const PurchaseView = (props) => {
 
-    let {match, getPurchases, getPurchaseSeller, getPurchaseBuyer, getSeller} = props;
+    let {match, getPurchases, getPurchaseSeller, getPurchaseBuyer} = props;
 
     const purchase = useSelector((state) => state.purchase);
     const user = useSelector((state) => state.auth.user);
@@ -22,34 +22,21 @@ const PurchaseView = (props) => {
 
     //getPurchases();
 
-
     const onClickGetSeller = () => {
         getPurchases();
 
-        console.log("MY PURCHASES:"+JSON.stringify(purchase?.purchases?.purchases));
-
-        console.log("MY SELLER BUYER:"+JSON.stringify(purchase?.purchases?.purchases?.filter(p => p.seller == user._id)));
-
+        //TODO:purchase.seller state ine yazıyor ama anlık data çekilemiyor state den
+        //purchase.purchases.purchases.filter(p => p.seller == user._id).map( p => getPurchaseSeller(p.seller));
         purchase.purchases.purchases.filter(p => p.seller == user._id).map( p => props.history.push("../bargain/"+p._id));
-
-        console.log("sellerrr:"+JSON.stringify(purchase.seller));
-
     }
 
     const onClickGetBuyer = () => {
         getPurchases();
 
-        console.log("MY PURCHASES:"+JSON.stringify(purchase?.purchases?.purchases));
-
-        console.log("MY SELLER BUYER:"+JSON.stringify(purchase?.purchases?.purchases?.filter(p => p.buyer == user._id)));
-
+        //TODO:purchase.buyer state ine yazıyor ama anlık data çekilemiyor state den
+        //purchase.purchases.purchases.filter(p => p.buyer == user._id).map( p => getPurchaseBuyer(p.buyer));
         purchase.purchases.purchases.filter(p => p.buyer == user._id).map( p => props.history.push("../bargain/"+p._id));
-
-        console.log("buyerrr:"+JSON.stringify(purchase.buyer));
-
     }
-
-    //console.log("sellerrr:"+JSON.stringify(purchase.seller));
 
 
     return(
@@ -57,10 +44,34 @@ const PurchaseView = (props) => {
             <Header/>
             <h1 className="myGarage text-center">My Purchases</h1>
             <div className="d-block justify-content-center" style={{margin: 100}}>
-                <FormLabel>I am the Seller</FormLabel><br/>
                 <Button onClick={onClickGetSeller}>Go To My Sales</Button><br/><br/>
-                <FormLabel>I am the Buyer</FormLabel><br/>
-                <Button onClick={onClickGetBuyer}>Go To My Orders</Button>
+                {purchase?.purchases?.purchases?.filter(p => p.seller == user._id).map( p => p.selectedItemList.map( (item) => {
+                    return (
+                    <GarageItem
+                    name={item.name}
+                    info={item.info}
+                    tags={item.tags}
+                    price={item.price}
+                    button1Name={"Buy"}
+                    button2Name={"Bargain"}
+                    condition={true}
+                    userView={true}
+                    />
+                    )}))} <br/><br/>
+                <Button onClick={onClickGetBuyer}>Go To My Orders</Button><br/><br/>
+                {purchase?.purchases?.purchases?.filter(p => p.buyer == user._id).map( p => p.selectedItemList.map( (item) => {
+                    return (
+                        <GarageItem
+                            name={item.name}
+                            info={item.info}
+                            tags={item.tags}
+                            price={item.price}
+                            button1Name={"Buy"}
+                            button2Name={"Bargain"}
+                            condition={true}
+                            userView={true}
+                        />
+                    )}))}
             </div>
 
         </div>
@@ -69,6 +80,6 @@ const PurchaseView = (props) => {
 
 }
 
-export default connect(null, { getPurchases, getPurchaseSeller, getPurchaseBuyer, getSeller})(withRouter(
+export default connect(null, { getPurchases, getPurchaseSeller, getPurchaseBuyer})(withRouter(
     PurchaseView));
 
